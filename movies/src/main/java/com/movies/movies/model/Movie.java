18ef27be45658a5dc2813bdc6d04524cf22fce92
Nikola <n.maric1912@gmail.com>
2024-04-15 10:35:04 +0200

    Add spring app

---
+++ movies/src/main/java/com/movies/movies/model/Movie.java
@@ -0,0 +1,17 @@
+package com.movies.movies.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "sv_69_2021")
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String genre;
+    private Long year;
+}
